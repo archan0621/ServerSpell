3ea67275b55dec916da95d689f37493609ef68fe
archan0621 <joshua_0621@naver.com>
2022-11-02 01:38:43 +0000

    add sort function and -h, -v command

--- ServerSpell.c
+++ ServerSpell.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
-#include <string.h>`
+#include <string.h>
 #include <stdlib.h>
+
+int sort(char *option);
 
 int main(int argc, char* argv[]){
 
 	if(argc < 2){
-		printf("옵션을 입력해주세요\n");
+		printf("ServerSpell v1.0.0 : no command specified\nTry \'ServerSpell -h\' for more information.\n");
 	}else{
-		if(strcmp(argv[1],"-h") == 0){
-			printf(" this is a help \n");
-		}else{
-			for(int i=0; i < argc ; i++){
-                		printf("argv[%d]은 %s입니다.\n",i,argv[i]);
-        	}
-        		printf("argc는 %d개 입니다.\n", argc);
-		}
+		sort(argv[1]);
+	}
+	return 0;
+}
+
+int sort(char *option){
+
+	if(strncmp(option, "-h",sizeof(option))==0){
+		printf("ServerSpell v1.0.0\n\nCommands\n\n-h : show command option information\n\n-v : show ServerSpells Version\n\n");
+	}else if(strncmp(option,"-v",sizeof(option))==0){
+		printf("\nServerSpell Version 1.0.0 Develop by Archan All right Reversed\n\n");
+	}else{
+		printf("Not invalid Option\n");
 	}
 
 	return 0;
-	
 }
